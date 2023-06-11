24b5fb5bf68137d81a0f2274a8700abc28f30d6c
ShivamGarg30304 <99480259+ShivamGarg30304@users.noreply.github.com>
2023-06-11 17:21:06 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+void quicksort(){
+	// This is the function for the quicksort
+}
+
+void mergeSort(){
+	// Merge Sort Function...
+}
+
+void bubbleSort(){
+	// Bubble Sort Function...
+}
+
+int main(){
+	cout << "Hello World" << endl;
+	cout << "Hello dot Batch" << endl;
+	return 0;
+}
